e31b5a74d3b8be092b9ae6e758fabd20faa8d053
chandank013 <ck263022@gmail.com>
2025-10-22 20:46:34 +0530

    login/signup

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -45,7 +45,7 @@ export async function POST(request: NextRequest) {
       email: user.email,
     };
 
-    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
       expiresIn: "1d",
     });
 
